a9c01d05117e7a68d43f392c06738e3dfd4caa9b
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-25 11:11:53 +0700

    define SMSBroadCastReceiver class and config AndroidManifest.xml

--- SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/MainActivity.java
+++ SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/MainActivity.java
@@ -11,8 +11,6 @@ import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static String SMS_BUNDLE = "pdus";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,30 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void initDisplayListSMSMessage()
     {
-        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Bundle intentExtras = intent.getExtras();
-                if (intentExtras != null) {
-                    Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
-                    String smsMessageStr = "";
-                    for (int i = 0; i < sms.length; ++i) {
-                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
-
-                        String smsBody = smsMessage.getMessageBody().toString();
-                        String address = smsMessage.getOriginatingAddress();
-
-                        smsMessageStr += "SMS From: " + address + "\n";
-                        smsMessageStr += smsBody + "\n";
-                    }
-                    Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
-
-        IntentFilter intentFilter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
-        //thong bao cho He thong biet dung SMS_RECEIVED_ACTION de nhan tin nhan thong qua BroadCastReceiver
-        this.registerReceiver(broadcastReceiver, intentFilter);
+//        IntentFilter intentFilter = new IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION);
+//        //thong bao cho He thong biet dung SMS_RECEIVED_ACTION de nhan tin nhan thong qua BroadCastReceiver
+//        this.registerReceiver(broadcastReceiver, intentFilter);
 
         //ListSMSMessageAdapter listSMSMessageAdapter = new ListSMSMessageAdapter((Context)this, R.layout.activity_main );
     }
---
+++ SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/SMSBroadCastReceiver.java
@@ -0,0 +1,34 @@
+package trungnt.mad3.techkids.smsmessage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by TrungNT on 4/25/2016.
+ */
+public class SMSBroadCastReceiver extends BroadcastReceiver {
+    public static String SMS_BUNDLE = "pdus";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                String smsBody = smsMessage.getMessageBody().toString();
+                String address = smsMessage.getOriginatingAddress();
+
+                smsMessageStr += "SMS From: " + address + "\n";
+                smsMessageStr += smsBody + "\n";
+            }
+            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
