e5a95df5ed5b299f96f4c6de26c3cc09c9bf422e
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-25 12:08:07 +0700

    define Notification SMS Message (temp)

--- SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/SMSBroadCastReceiver.java
+++ SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/SMSBroadCastReceiver.java
@@ -1,9 +1,13 @@
 package trungnt.mad3.techkids.smsmessage;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
@@ -25,10 +29,27 @@ public class SMSBroadCastReceiver extends BroadcastReceiver {
                 String smsBody = smsMessage.getMessageBody().toString();
                 String address = smsMessage.getOriginatingAddress();
 
-                smsMessageStr += "SMS From: " + address + "\n";
-                smsMessageStr += smsBody + "\n";
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(context)
+                                .setSmallIcon(R.drawable.message)
+                                .setContentTitle(address)
+                                .setContentText(smsBody);
+
+                Intent resultIntent = new Intent(context, MainActivity.class);
+
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+                stackBuilder.addParentStack(MainActivity.class);
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(
+                                0,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                mBuilder.setContentIntent(resultPendingIntent);
+                NotificationManager mNotificationManager =
+                        (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(9999, mBuilder.build());
             }
-            Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
         }
     }
 }
