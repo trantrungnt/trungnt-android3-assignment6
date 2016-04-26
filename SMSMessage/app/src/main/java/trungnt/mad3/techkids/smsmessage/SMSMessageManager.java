8b25b9cefa6c8162199dcbcfcddb4e1ed6eb19a9
Trần Trung <nguyentrantrung1986@gmail.com>
2016-04-26 15:39:21 +0700

    define SMSMessageManager class

--- SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/SMSMessageManager.java
+++ SMSMessage/app/src/main/java/trungnt/mad3/techkids/smsmessage/SMSMessageManager.java
@@ -2,9 +2,26 @@ package trungnt.mad3.techkids.smsmessage;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+
 /**
  * Created by TrungNT on 4/26/2016.
  */
 public class SMSMessageManager extends Application {
-    
+    private static SMSMessageManager ourInstance = new SMSMessageManager();
+    private ArrayList<SMSMessage> arrListSMSMessage = new ArrayList<>();
+
+    public ArrayList<SMSMessage> getArrSMSMessage() {
+        return arrListSMSMessage;
+    }
+
+    public static SMSMessageManager getInstance() {
+        return ourInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ourInstance = this;
+    }
 }
